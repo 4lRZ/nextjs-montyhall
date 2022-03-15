4bf19bacb638282f7af02d23c6bd4e5a3e54a4e3
Alvaro Dormundo <adormundo@gmail.com>
2022-03-15 13:09:51 -0300

    Integrando Router + Form

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,21 +1,33 @@
-import { useState } from "react";
-import Porta from "../components/Porta";
-import { atualizarPortas, criarPortas } from "../functions/portas"
-import PortaModel from "../model/porta";
+import Card from "../components/Card"
+import EntradaNumerica from "../components/EntradaNumerica"
+import styles from "../styles/Form.module.css"
+import Link from "next/link"
+import { useState } from "react"
 
-export default function Home() {
-  const [portas, setPortas] = useState(criarPortas(3, 2))
-
-  function renderizarPortas() {
-    return portas.map(porta => {
-      return <Porta key={porta.numero} value={porta} onChange={novaPorta => setPortas(atualizarPortas(portas, novaPorta))}/>
-    })
-  }
+export default function Form() {
+  const [qtdePortas, setQtdePortas] = useState(3)
+  const [comPresente, setComPresente] = useState(1)
 
   return (
-    <div style={{display: "flex",}}>
-      {renderizarPortas()}
+    <div className={styles.form}>
+      <div>
+        <Card bgcolor="#c0392c">
+          <h1 className={styles.logo}>Monty Hall</h1>
+        </Card>
+        <Card>
+          <EntradaNumerica text="Quantidade de Portas:" value={qtdePortas} onChange={novaQtde => setQtdePortas(novaQtde)}></EntradaNumerica>
+        </Card>
       </div>
-      
+      <div>
+        <Card>
+        <EntradaNumerica text="Porta Premiada:" value={comPresente} onChange={novaPortaComPresente => setComPresente(novaPortaComPresente)}></EntradaNumerica>
+        </Card>
+        <Card bgcolor="#28a085">
+          <Link href={`/jogo/${qtdePortas}/${comPresente}`} passHref>
+            <h2 className={styles.link}>Iniciar</h2>
+          </Link>
+        </Card>
+      </div>
+    </div>
   )
 }
